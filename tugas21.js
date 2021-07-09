16dd08fc4b4313fee3ad14c0e9ff982a888ba6f9
Saddam Husein <saddambinhusein2@gmail.com>
2021-07-09 23:44:01 +0800

    Tugas 21 JS Pengumpulan tugas 21 JS ke Niomic

---
+++ tugas21.js
@@ -0,0 +1,9 @@
+function nasiGoreng() {
+    let data = "Saya belajar masak nasi goreng";
+    console.log(data);
+
+    let dataBaru = data.replace('masak', 'makan');
+    console.log(dataBaru);
+}
+
+nasiGoreng();
